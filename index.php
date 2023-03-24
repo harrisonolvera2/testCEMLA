36782b150019f8d6746de3146fc633001e0de567
Harrison Olvera Calleja <harrisonolvera2@gmail.com>
2023-03-24 16:54:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,90 @@
 <?php
 
-// Definir los días de pago
-$pay_days = [10, 25];
+// Fecha de inicio y fin del año en curso
+$fecha_inicio = date("Y") . "-01-01";
+$fecha_fin    = date("Y") . "-12-31";
 
-// Obtener el año actual
-$current_year = date('Y');
+// Arreglo de días de pago
+$dias_pago = array(10, 25);
 
-// Definir la matriz para almacenar las fechas de pago
-$pay_dates = [];
+// Arreglo de días de fin de semana
+$fin_de_semana = array("Sat", "Sun");
 
-// Recorrer los meses del año
-for ($month = 1; $month <= 12; $month++) {
+// Arreglo de pagos por mes
+$pagos_mes = array();
 
-    // Recorrer los días de pago
-    foreach ($pay_days as $pay_day) {
+// Arreglo de pagos por quincena
+$pagos_quincena = array();
 
-        // Obtener la fecha de pago del mes actual
-        $pay_date = date("$current_year-$month-$pay_day");
+// Arreglo de pagos por bimestre
+$pagos_bimestre = array();
 
-        // Verificar si la fecha de pago cae en fin de semana
-        $weekday = date('w', strtotime($pay_date));
-        if ($weekday == 0) { // Domingo
-            $pay_date = date('Y-m-d', strtotime("$pay_date -2 days")); // Pagar el viernes anterior
-        } elseif ($weekday == 6) { // Sábado
-            $pay_date = date('Y-m-d', strtotime("$pay_date -1 day")); // Pagar el viernes anterior
+// Variable para el conteo de quincenas
+$contador_quincenas = 0;
+
+// Variable para la fecha actual
+$fecha_actual = $fecha_inicio;
+
+// Ciclo para calcular los días de pago del año en curso
+while ($fecha_actual <= $fecha_fin) {
+    $dia_semana = date("D", strtotime($fecha_actual));
+    $dia_mes    = date("j", strtotime($fecha_actual));
+
+    if (in_array($dia_mes, $dias_pago)) {
+        if (in_array($dia_semana, $fin_de_semana)) {
+            $fecha_pago = date("Y-m-d", strtotime($fecha_actual . "last weekday"));
+        } else {
+            $fecha_pago = $fecha_actual;
         }
 
-        // Agregar la fecha de pago a la matriz
-        $pay_dates[] = $pay_date;
+        $mes_pago      = date("m", strtotime($fecha_pago));
+        $quincena_pago = ceil(date("j", strtotime($fecha_pago)) / 15);
+        $bimestre_pago = ceil($mes_pago / 2);
+
+        $pagos_mes[$mes_pago][]           = $fecha_pago;
+        $pagos_quincena[$quincena_pago][] = $fecha_pago;
+        $pagos_bimestre[$bimestre_pago][] = $fecha_pago;
+
+        // Verificar si es la última quincena del bimestre
+        if ($quincena_pago == 2 && $mes_pago % 2 == 0 && date("t", strtotime($fecha_pago)) - date("j", strtotime($fecha_pago)) < 15) {
+            $ultima_quincena_bimestre = true;
+        } else {
+            $ultima_quincena_bimestre = false;
+        }
     }
+
+    
+    $fecha_actual = date("Y-m-d", strtotime($fecha_actual . "+ 1 day"));
 }
 
-// Ordenar las fechas de pago de forma ascendente
-//sort($pay_dates);
-
-// Imprimir las fechas de pago
-echo "Fechas de pago para el año $current_year:\n";
-foreach ($pay_dates as $pay_date) {
-    echo $pay_date."<br>";
+// Imprimir los pagos por mes
+echo "Pagos por mes:<br>";
+foreach ($pagos_mes as $mes => $pagos) {
+    echo "Mes " . $mes . ":<br>";
+    foreach ($pagos as $pago) {
+        echo "- " . $pago . "<br>";
+    }
+    echo "<br>";
 }
+
+// Imprimir los pagos por quincena
+echo "Pagos por quincena:<br>";
+foreach ($pagos_quincena as $quincena => $pagos) {
+    echo "Quincena " . $quincena . ":<br>";
+    foreach ($pagos as $pago) {
+        echo "- " . $pago . "<br>";
+    }
+    echo "<br>";
+}
+
+// Imprimir los pagos por bimestre
+echo "Pagos por bimestre:<br>";
+foreach ($pagos_bimestre as $bimestre => $pagos) {
+    echo "Bimestre " . $bimestre . ":<br>";
+    foreach ($pagos as $pago) {
+        echo "- " . $pago . "<br>";
+    }
+    echo "<br>";
+}
+
 ?>
